676cec15e3f8b32e1ea5e9fc500e1bd463537398
romamotwani02 <166395387+romamotwani02@users.noreply.github.com>
2024-09-02 18:08:57 +0530

    Create 9)star_diamond.py
      *
     ***
    *****
    *****
     ***
      *

---
+++ pattern/9)star_diamond.py
@@ -0,0 +1,19 @@
+def nStarDiamond(n: int) -> None:
+    # Write your code here.
+    for i in range(n):
+        for j in range(n-i-1):
+            print(" ",end="")
+        for k in range(i*2+1):
+            print("*",end="")
+        for l in range(n-i-1):
+            print(" ",end="")
+        print()
+    for j in range(n-1,-1,-1):
+        for i in range(n-j-1):
+            print(" ",end="")
+        for l in range(j*2+1):
+            print("*",end="")
+        for k in range(n-j-1):
+            print(" ",end="")
+        print()
+    pass
