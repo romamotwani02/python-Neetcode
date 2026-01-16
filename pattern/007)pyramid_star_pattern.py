2e6d3c0d00d304e28035070aa569f5f90c198ad4
romamotwani02 <166395387+romamotwani02@users.noreply.github.com>
2026-01-16 19:02:38 +0530

    Update 007)pyramid_star_pattern.py

--- pattern/007)pyramid_star_pattern.py
+++ pattern/007)pyramid_star_pattern.py
@@ -10,3 +10,13 @@ def nStarTriangle(n: int) -> None:
         for i in range((n-i-1)):
             print(" ",end="")
         print()
+
+
+#another way
+rows = 5
+for i in range(1, rows + 1):
+ left_spaces = rows i
+ stars = 2*1-1
+ right_spaces = rows - i
+
+ print(" * * left_spaces + "*" * stars + " " * right_spaces)
