d2534d52aec38b2c5076a13a3ca6921d63381147
romamotwani02 <166395387+romamotwani02@users.noreply.github.com>
2026-01-16 10:11:40 +0530

    Update 004)repeated number triangle.py

--- pattern/004)repeated number triangle.py
+++ pattern/004)repeated number triangle.py
@@ -1,7 +1,7 @@
 def triangle(n:int) ->None:
     for i in range(n):
-        for j in range(i):
-            print(i,end=" ")
+        for j in range(i+1):
+            print(i+1,end=" ")
         print()
 
 #another way
