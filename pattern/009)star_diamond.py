06bee505e57a636fe6a98624cb4b285a57b3e772
romamotwani02 <166395387+romamotwani02@users.noreply.github.com>
2026-01-22 01:10:37 +0530

    Update 009)star_diamond.py

--- pattern/009)star_diamond.py
+++ pattern/009)star_diamond.py
@@ -17,3 +17,20 @@ def nStarDiamond(n: int) -> None:
             print(" ",end="")
         print()
     pass
+
+#other way
+n=5
+
+for i in range(0,n):
+ space=n-i-1
+ star=i*2+1
+ print(' '*space+'*'*star+' '*space)
+for i in range(n-1,-1,-1):
+ star2=i*2+1
+ space2=n-i-1
+ print(' '*space2+'*'*star2+' '*space2)
+
+
+
+
+
