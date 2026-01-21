730244853594c2515182db1a5224d06b218b65da
romamotwani02 <166395387+romamotwani02@users.noreply.github.com>
2026-01-22 00:57:45 +0530

    Update 008)inverted_pyramid_star_pattern.py

--- pattern/008)inverted_pyramid_star_pattern.py
+++ pattern/008)inverted_pyramid_star_pattern.py
@@ -8,3 +8,11 @@ def nStarTriangle(n: int) -> None:
     for l in range(n-i-1):
         print(" ",end="")
     print()
+    
+    
+#other way
+n=5
+for i in range(n-1,-1,-1):
+ space=n-i-1
+ star=i*2+1
+ print('  '*space+"*"*star+'  '*space)
