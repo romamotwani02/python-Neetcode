752d960942105347df1b81bc189b69e02c3084f9
romamotwani02 <166395387+romamotwani02@users.noreply.github.com>
2026-01-28 15:30:10 +0530

    Update 012)Number_crown.py

--- pattern/012)Number_crown.py
+++ pattern/012)Number_crown.py
@@ -18,3 +18,16 @@ def numberCrown(n: int) -> None:
         print()
         space=-2
     pass
+
+#other way
+n=4
+for i in range(1,n+1):
+ for j in range(1,i+1):
+  print(j,end='')
+ spaces=2*(n-i)
+ for k in range(1, spaces+1):
+  print(" ",end='')
+ for l in range(i,0,-1):
+  print(l,end='')
+  print()
+
