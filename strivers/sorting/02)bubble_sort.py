20deb46f70c80cc2ac4e729e6941c53961f251d4
romamotwani02 <166395387+romamotwani02@users.noreply.github.com>
2026-02-05 19:19:59 +0530

    Update 02)bubble_sort.py

--- strivers/sorting/02)bubble_sort.py
+++ strivers/sorting/02)bubble_sort.py
@@ -12,3 +12,18 @@ def bubbleSort(arr1,size):
 
 arr1=[13,46,24,52,20, 9]
 arr2=bubbleSort(arr1,len(arr1))
+
+
+#recent method that I followed
+def bubblesort (arr, size) :
+ for i in range(size-2, -1,-1):
+  for j in range(0,i+1):# it is actually i-2
+   if(arr [j]›arr[j+1]):
+     arr[jl, arr[j+1]=arr[j+1], arr[i]
+     print(arr)
+   print('final sorted', arr)
+
+ arr=[5,8, 1, 6,9,2,4] 
+ size=len(arr)
+ bubblesort (arr, size)
+
